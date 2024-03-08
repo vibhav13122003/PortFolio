db34021f2f8d701c044e6c9c381b7dfda7f7b4aa
vibhav13122003 <vibhavbhartiya13@gmail.com>
2024-03-08 22:12:51 +0530

    make about responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,35 @@ import {
   faNodeJs,
   faJava,
   faNode,
+  
 } from "@fortawesome/free-brands-svg-icons";
-import { faWind } from "@fortawesome/free-solid-svg-icons";
+import { faWind, faBriefcase } from "@fortawesome/free-solid-svg-icons";
+const timelineData = [
+  {
+    timePeriod: "Feb'23 - Present",
+    position: "Member",
+    company: "FOSSCU",
+    about: `People at FOSSCU (Free and Open-Source Community United ) are a Group of individuals who are constantly working to promote Open-Source Culture.`,
+  },
+  {
+    timePeriod: "March'23 - Present",
+    position: "Member (Web Domain)",
+    company: "GDSC KIET",
+    about: `a club open to all undergraduate and graduate students with an interest in learning about what it means to be a developer. By joining our chapter, club members learn what it means to form the bridge between the theory and concepts taught in classes and practice with industry developer tools.`,
+  },
+  {
+    timePeriod: "March'23 - Nov'23",
+    position: "Web Developer",
+    company: "Ambuvians HealthCare pvt. ltd.",
+    about: `your destination for healthcare and emergency medical services. Our mission is to provide exceptional care promptly and empathetically. As a dedicated ambulance service, we offer rapid assistance to those in need. Led by skilled medical professionals and advanced technology, we lead in medical transportation and emergency response. Our unwavering commitment to excellence makes us a reliable choice during crises.`,
+  },
+  {
+    timePeriod: "Dec'22 - July'23",
+    position: "Member",
+    company: "Innogeeks KIET",
+    about: `a remarkable joint initiative by both faculty and students, strives to elevate our institution and align it with esteemed national and international counterparts. - Focussing on empowering students to excel beyond their academic knowledge, honing their skills for real-world challenges.`,
+  },
+];
 
 const About = () => {
   return (
@@ -151,16 +178,20 @@ const About = () => {
       <div className='thirdContainer'>
         <div className='work-experience'>
           <h2>Work Experience</h2>
-
-          <ul>
-            <li>
-              <strong>Company A</strong> - Position A (Year A - Year B)
-            </li>
-            <li>
-              <strong>Company B</strong> - Position B (Year C - Year D)
-            </li>
-            {/* Add more experiences as needed */}
-          </ul>
+          <div className='timeline'>
+            {timelineData.map((myTimeline, index) => (
+              <div key={index} className='timeline-item'>
+                <div className='tl-icon'>
+                  <FontAwesomeIcon icon={faBriefcase} />
+                </div>
+                <p className='tl-duration'>{myTimeline.timePeriod}</p>
+                <h5>
+                  {myTimeline.position} <span> - {myTimeline.company}</span>
+                </h5>
+                <p className='tl-para'>{myTimeline.about}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
